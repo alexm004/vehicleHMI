4105b0abffd8d239c8a992f79ccf3f353154186a
quest <alex.mpaul@quest-global.com>
2022-01-05 21:03:16 +0530

    Tabs added

--- app/src/main/java/com/example/vehiclehmi/Control.java
+++
@@ -1,4 +0,0 @@
-package com.example.vehiclehmi;
-
-public class Control {
-}
--- app/src/main/java/com/example/vehiclehmi/MainActivity.java
+++ app/src/main/java/com/example/vehiclehmi/MainActivity.java
@@ -1,17 +1,59 @@
 package com.example.vehiclehmi;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
+
+
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+    TabLayout tabLayout;
+    ViewPager2 viewPager2;
+    FragmentAdapter FragmentAdapter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tabLayout =  findViewById(R.id.tabLayout);
+        viewPager2 =  findViewById(R.id.viewPager2);
+        FragmentManager fm = getSupportFragmentManager();
 
+        FragmentAdapter = new FragmentAdapter(fm, getLifecycle());
+        viewPager2.setAdapter(FragmentAdapter);
+
+
+        tabLayout.addTab(tabLayout.newTab().setText("Controls"));
+        tabLayout.addTab(tabLayout.newTab().setText("Settings"));
+
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
 
     }
 }
