7a702b9eab9d37b38a1b2a487ef90dd4f8575527
quest <alex.mpaul@quest-global.com>
2022-01-13 00:45:58 +0530

    aidl in hmi

---
+++ app/src/main/aidl/ServicePackage/aidlInterface.aidl
@@ -0,0 +1,10 @@
+// aidlInterface.aidl
+package ServicePackage;
+
+// Declare any non-default types here with import statements
+
+interface aidlInterface {
+
+    int basicTypes(int anInt, long aLong, boolean aBoolean, float aFloat,
+            double aDouble, String aString);
+}
--- app/src/main/java/com/example/vehiclehmi/MainActivity.java
+++ app/src/main/java/com/example/vehiclehmi/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.vehiclehmi;
 
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.os.IBinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,10 +20,16 @@ import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.android.material.tabs.TabLayout;
 
+import java.util.List;
+
+import ServicePackage.aidlInterface;
+
 public class MainActivity extends AppCompatActivity {
-    TabLayout tabLayout;
-    ViewPager2 viewPager2;
-    FragmentAdapter FragmentAdapter;
+
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager2;
+    private FragmentAdapter FragmentAdapter;
+    private aidlInterface aidlObject;
 
 
 
@@ -24,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         tabLayout =  findViewById(R.id.tabLayout);
         viewPager2 =  findViewById(R.id.viewPager2);
         FragmentManager fm = getSupportFragmentManager();
@@ -55,5 +69,42 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        bindtoAIDLService();
+
+    }
+
+    private void bindtoAIDLService() {
+
+        Intent aidlServiceIntent = new Intent("connect_to_aidl_service");
+        bindService(implicitIntentToExplicitIntent(aidlServiceIntent,this),serviceConnectionObject,BIND_AUTO_CREATE);
+    }
+
+    ServiceConnection serviceConnectionObject = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+
+            aidlObject = aidlInterface.Stub.asInterface(service);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
+
+    public static Intent implicitIntentToExplicitIntent(Intent implicitIntent, Context context) {
+        PackageManager pm = context.getPackageManager();
+        List<ResolveInfo> resolveInfoList = pm.queryIntentServices(implicitIntent, 0);
+
+        if (resolveInfoList == null || resolveInfoList.size() != 1) {
+            return null;
+        }
+
+        ResolveInfo serviceInfo = resolveInfoList.get(0);
+        ComponentName component = new ComponentName(serviceInfo.serviceInfo.packageName, serviceInfo.serviceInfo.name);
+        Intent explicitIntent = new Intent(implicitIntent);
+        explicitIntent.setComponent(component);
+        return explicitIntent;
     }
 }
